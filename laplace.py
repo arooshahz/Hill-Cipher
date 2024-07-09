2f1976c4bfdfd74c9774539e7b1c115c86f50da3
arooshahz <aroosha.ha@gmail.com>
2024-07-09 21:30:43 +0330

    changes in laplace method

--- laplace.py
+++ laplace.py
@@ -1,27 +1,41 @@
 def get_minor(matrix, row, col):
-    minor = []
-    for i in range(len(matrix)):
-        if i != row:
-            new_row = []
-            for j in range(len(matrix[i])):
-                if j != col:
-                    new_row.append(matrix[i][j])
-            minor.append(new_row)
-    return minor
+    return [
+        [matrix[i][j] for j in range(len(matrix)) if j != col]
+        for i in range(len(matrix)) if i != row
+    ]
 
 
-def LaplaceExpansion(matrix):
-    if len(matrix[0]) == 2:
+def LaplaceExpansion(matrix, memo={}):
+    key = tuple(map(tuple, matrix))
+    if key in memo:
+        return memo[key]
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
     det = 0
     for i in range(len(matrix[0])):
         minor = get_minor(matrix, 0, i)
-        det += ((-1) ** i) * matrix[0][i] * LaplaceExpansion(minor)
+        det += ((-1) ** i) * matrix[0][i] * LaplaceExpansion(minor, memo)
+
+    memo[key] = det
     return det
 
+try:
+    n = int(input().strip())
+    matrix = []
 
-matrix = [[1, 2, 3],
-          [0, 5, 6],
-          [0, 8, 9]]
+    for _ in range(n):
+        row = list(map(float, input().strip().split()))
+        if len(row) != n:
+            raise ValueError(f"Each row must contain {n} elements.")
+        matrix.append(row)
 
-print(f'Determinant: {LaplaceExpansion(matrix)}')
+    det = LaplaceExpansion(matrix)
+
+    print(int(det))
+
+except ValueError as e:
+    print("Invalid input:", str(e))

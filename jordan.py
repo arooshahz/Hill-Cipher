840ca2271e36392b55a01e4ad4ffebd3250d3657
arooshahz <aroosha.ha@gmail.com>
2024-07-09 21:32:48 +0330

    changes in jordan method

--- jordan.py
+++ jordan.py
@@ -1,21 +1,21 @@
 def jordan(matrix):
     n = len(matrix)
-    A = [row[:] for row in matrix] 
+    A = [row[:] for row in matrix]
     det = 1
 
     for i in range(n):
         if A[i][i] == 0:
             for j in range(i + 1, n):
                 if A[j][i] != 0:
-                    A[i], A[j] = A[j], A[i] 
-                    det *= -1  
+                    A[i], A[j] = A[j], A[i]
+                    det *= -1
                     break
             else:
-                return 0  
+                return 0
 
         det *= A[i][i]
         for k in range(i + 1, n):
-            A[i][k] /= A[i][i]  
+            A[i][k] /= A[i][i]
         A[i][i] = 1
 
         for j in range(i + 1, n):
@@ -25,11 +25,28 @@ def jordan(matrix):
 
     return det
 
-matrix = [
-    [1, 2, 3],
-    [0, 5, 6],
-    [0, 8, 9]
-]
 
-det = jordan(matrix)
-# print(f'Determinant: {det}')
+def floor(x):
+    return int(x // 1) if x >= 0 else int(x // 1) - 1
+
+
+
+try:
+    n = int(input().strip())
+    matrix = []
+
+    for _ in range(n):
+        row = list(map(float, input().strip().split()))
+        if len(row) != n:
+            raise ValueError(f"Each row must contain {n} elements.")
+        matrix.append(row)
+
+    det = jordan(matrix)
+
+    print(int(det))
+
+except ValueError as e:
+    print("Invalid input:", str(e))
+
+
+
